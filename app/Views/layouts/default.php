440b3b19ce526763037d89dbf499c0a346d668b4
Mr-Zero272 <pitithuong@gmail.com>
2022-12-08 15:15:22 +0700

    update header voi mot so thu

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -16,7 +16,8 @@
 
     <link rel="stylesheet" type="text/css" href="assets/css/slick.css">
     <link rel="stylesheet" type="text/css" href="assets/css/slick-theme.css">
-
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <!-- CSS only -->
     <title>ShoesStore</title>
@@ -45,15 +46,16 @@
     </div>
 
     <!-- Header section -->
-
     <?= $this->insert('layouts/header') ?>
 
-        <!-- Content section -->
-        <?= $this->section('page') ?>
+    <!-- Content section -->
+    <?= $this->section('page') ?>
 
-            <!-- Footer section -->
-            <?= $this->insert('layouts/footer') ?>
+    <!-- Footer section -->
+    <?= $this->insert('layouts/footer') ?>
 
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/scrollreveal"></script>
     <script src="assets/js/Main.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -90,7 +92,7 @@
     <!-- insert specific page's scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-    <script src="<?= request()->baseUrl(); ?>/assets/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="<?= request()->baseUrl(); ?>/assets/js/bootstrap.bundle.min.js"></script> -->
     <script src="<?= request()->baseUrl(); ?>/assets/js/tiny-slider.js"></script>
     <script src="<?= request()->baseUrl(); ?>/assets/js/glightbox.min.js"></script>
     <!-- <script src="<?= request()->baseUrl(); ?>/assets/js/sweetalert2.all.min.js"></script> -->
--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -3,30 +3,38 @@
     <i class='bx bx-chevron-up scrollTop_icon'></i>
 </a>
 <!----Header!---->
-<header class="l-header" id="header">
-    <nav class="nav bd-container">
-        <div class="nav__toggle" id="nav-toggle">
-            <i class='bx bx-menu'></i>
-        </div>
-        <a href="index.html" class="nav__logo">ShoesStore</a>
-        <div class="nav__menu" id="nav-menu">
-            <ul class="nav__list">
-                <li class="nav__item"><a href="/home" class="nav__link active-link">Home</a></li>
-                <li class="nav__item"><a href="AboutUs.html" class="nav__link">About</a></li>
-                <li class="nav__item"><a href="/product" class="nav__link">Product</a></li>
-                <li class="nav__item"><a href="/contact" class="nav__link">Contact</a></li>
-            </ul>
-        </div>
-        <div class="nav_shop">
-            <a href="/cart" class="btn btn-outline-secondary">
-                <i class="bx bxs-cart"></i>
+<header>
+    <nav class="navbar fixed-top navbar-expand-sm navbar-light bg-light">
+        <div class="container">
+            <a href="/home" class="navbar-brand mb-0 h1">
+                ShoesStore
             </a>
-            <a href="#" class="nav__icon btn btn-outline-secondary">
-                <i class='bx bxs-user-circle'></i>
-            </a>
-            <div class="nav__shop-content">
-                <div class="btn-checkout">
-                    <li><i class='bx bxs-log-in-circle'><a href="login.html"> LOG IN</a></i></li>
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a href="/home" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/about" class="nav-link">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/product" class="nav-link">Product</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/contact" class="nav-link">Contact</a>
+                    </li>
+                </ul>
+                <div class="nav_shop ml-auto">
+                    <a href="/cart" class="btn btn-outline-secondary">
+                        <i class="bx bxs-cart"></i>
+                    </a>
+                    <a href="#" class="nav__icon btn btn-outline-secondary">
+                        <i class='bx bxs-user-circle'></i>
+                    </a>
                 </div>
             </div>
         </div>
